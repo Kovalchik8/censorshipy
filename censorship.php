308efe5f9d3e8774b6338495b1275e972efe719e
Kovalchk <koval4ik8@email.ua>
2019-03-20 16:31:54 +0200

    more code was improved, all checkboxes work properly

--- censorship.php
+++ censorship.php
@@ -15,27 +15,42 @@ class CENSOR_Plugin {
   // class constructor
   public function __construct() {
 
+    // define constants
     define('PLUGIN_DIR', dirname(__FILE__).'/' );
     define('TABLE_ROWS', 3);
     define('OPTIONS_FIELD_NAME', 'censorshipy-plugin');
 
-    // $this->fieldsCnt = 3;
-    // $this->optionsFieldName = 'censorshipy-plugin';
+    // variables
+    $this->settings = array();
+    $this->filterTypes = array(
+      'content' => 'content',
+      'title' => 'title',
+      'comments' => 'comments'
+    );
 
     // add styles
-    add_action('admin_print_styles', array($this, 'add_my_stylesheet') );
+    add_action('admin_print_styles', array($this, 'censorshipStyles') );
 
     // create custom plugin settings menu
     add_action('admin_menu', array($this, 'createCensorshipPlugin') );
 
-    // filtering
-    add_filter( 'the_content', array($this, 'censorshipContent') );
-    add_filter( 'the_title', array($this, 'censorshipTitle') );
-    add_filter( 'comment_text', array($this, 'censorshipComment') );
+    // form setting for filtering
+    $this->formSettings();
+
+    // filtering using wp hooks
+    add_filter( 'the_content', function($data) {
+      return $this->censorshipFilter($data, $this->filterTypes['content']);
+    } );
+    add_filter( 'the_title', function($data) {
+      return $this->censorshipFilter($data, $this->filterTypes['title']);
+    } );
+    add_filter( 'comment_text', function($data) {
+      return $this->censorshipFilter($data, $this->filterTypes['comments']);
+    } );
 
   }
 
-  public function add_my_stylesheet() {
+  public function censorshipStyles() {
     wp_enqueue_style( 'myCSS', plugins_url( '/censor.css', __FILE__ ) );
   }
 
@@ -64,32 +79,31 @@ class CENSOR_Plugin {
   }
 
   // plugin page content
-  public function censorshipSettingsPage() { 
-    include_once (PLUGIN_DIR . 'includes/plugin-page.php');
-   }
+  public function censorshipSettingsPage() { include_once (PLUGIN_DIR . 'includes/plugin-page.php'); }
 
-  // filter content from all posts
-  public function censorshipContent($content) {
-    $content = str_ireplace(get_option('option-one-left'), get_option('option-one-right'), $content);
-    $content = str_ireplace(get_option('option-two-left'), get_option('option-two-right'), $content);
-    $content = str_ireplace(get_option('option-three-left'), get_option('option-three-right'), $content);
-    return $content;
+  // fill the array with settings from the database
+  public function formSettings() {
+
+    for ($i = 1; $i <= TABLE_ROWS; $i++) {
+
+      array_push( $this->settings, array(
+        'left' => get_option( 'option-left-' . $i ),
+        'right' => get_option( 'option-right-' . $i ),
+        'title' => get_option('title-' . $i),
+        'content' => get_option('content-' . $i),
+        'comments' => get_option('comments-' . $i)
+      ));
+
+    }
+
   }
 
-  // filter title from all posts
-  public function censorshipTitle($title) {
-    $title = str_ireplace(get_option('option-one-left'), get_option('option-one-right'), $title);
-    $title = str_ireplace(get_option('option-two-left'), get_option('option-two-right'), $title);
-    $title = str_ireplace(get_option('option-three-left'), get_option('option-three-right'), $title);
-    return $title;
-  }
-
-  // filter all comments
-  public function censorshipComment( $comment_text ) {
-    $comment_text = str_ireplace(get_option('option-one-left'), get_option('option-one-right'), $comment_text);
-    $comment_text = str_ireplace(get_option('option-two-left'), get_option('option-two-right'), $comment_text);
-    $comment_text = str_ireplace(get_option('option-three-left'), get_option('option-three-right'), $comment_text);
-    return $comment_text;
+  public function censorshipFilter($data, $type) {
+    foreach($this->settings as $setting) {
+      if ($setting[$type] == 1 && $setting['left']) 
+        $data = str_ireplace($setting['left'], $setting['right'], $data);
+    }
+    return $data;
   }
     
 }
